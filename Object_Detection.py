c1e9290ab46c945493f222fbe105efe2f964958d
Moien Sohani <99538075+MoienSD@users.noreply.github.com>
2023-09-15 10:38:54 +0330

    Update Object_Detection.py

--- Object_Detection.py
+++ Object_Detection.py
@@ -24,15 +24,5 @@ class ObjectDetection:
 
         self.model.setInputParams(size=(self.image_size, self.image_size), scale=1/255)
 
-    def load_class_names(self, classes_path="dnn_model/classes.txt"):
-
-        with open(classes_path, "r") as file_object:
-            for class_name in file_object.readlines():
-                class_name = class_name.strip()
-                self.classes.append(class_name)
-
-        self.colors = np.random.uniform(0, 255, size=(80, 3))
-        return self.classes
-
     def detect(self, frame):
         return self.model.detect(frame, nmsThreshold=self.nmsThreshold, confThreshold=self.confThreshold)
